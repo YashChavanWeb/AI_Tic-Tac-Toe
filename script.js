8fb46be9350fea9287bcbb27fb081fa6f8ce502a
YashChavanWeb <yashchavan4628@gmail.com>
2023-12-12 00:00:19 +0530

    this is the complete code with the inclusion of ai

--- script.js
+++ script.js
@@ -1,149 +1,186 @@
-// Selecting DOM elements
-let boxes = document.querySelectorAll('.box'); // use selector all which I don't do and make a common mistake
-const playAgain = document.querySelector('.play-again');
-const message = document.querySelector('.message');
-const winner = document.querySelector('#winner');
-const newGame = document.querySelector('#new-game');
+document.addEventListener('DOMContentLoaded', () => {
+    const playerX = 'X';
+    const playerO = 'O';
+    let currentPlayer = playerX;
+    let gameBoard = ['', '', '', '', '', '', '', '', ''];
+    let gameOver = false;
 
-let play_x = true;
-let turn = "X";
+    const boxes = document.querySelectorAll('.box');
+    const messageDisplay = document.querySelector('.message');
+    const winnerDisplay = document.getElementById('winner');
+    const playAgainBtn = document.querySelector('.play-again');
+    const newGameBtn = document.getElementById('new-game');
+    const playerTypeSelect = document.getElementById('playerType');
 
-const winCondition = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
+    // Function to check if the current player has won
+    const checkWinner = () => {
+        const winPatterns = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+            [0, 4, 8], [2, 4, 6]             // Diagonals
+        ];
 
-boxes.forEach((box) => {
-    box.addEventListener('click', () => {
-        console.log("Box was clicked");
-
-        if (play_x) { // X turn
-            box.textContent = "X";
-            box.style.transition = "0.5s ease";
-            play_x = false;
-        } else { // O turn
-            box.textContent = "O";
-            box.style.transition = "0.5s ease";
-            play_x = true;
-        }
-        box.disabled = true; // this will disable the button once clicked, very useful
-
-        // Note: this also changes the text / background color so we need to change it in style
-
-        // Now after each turn we need to check whether the user has won or not
-        box.classList.add('clicked'); // Add the 'clicked' class for the flip effect
-        count++;
-
-        changeBg();
-        isWinner();
-    });
-});
-
-function changeBg() {
-    if (turn === "X") {
-        turn = "O";
-        document.querySelector('#two').style.backgroundColor = "white";
-        document.querySelector('#one').style.backgroundColor = "#DC143C";
-    } else {
-        turn = "X";
-        document.querySelector('#one').style.backgroundColor = "white";
-        document.querySelector('#two').style.backgroundColor = "#DC143C";
-    }
-}
-
-const displayWinner = (tell) => {
-    winner.innerText = `Winner is ${tell}`; // Use the parameter winnerText
-    message.classList.remove("hide");
-    stopButtons();
-};
-
-const stopButtons = () => {
-    for (let box of boxes) {
-        box.disabled = true;
-    }
-};
-
-const startButtons = () => {
-    for (let box of boxes) {
-        box.disabled = false;
-        box.textContent = "";
-    }
-};
-
-let count = 0;
-
-const isWinner = () => {
-    for (let condition of winCondition) {
-        // the condition that is in the loop is basically an array of all the patterns of 0, 1, ... index
-
-
-
-
-        /*
-        console.log(condition)     // we get array of all the indexes 
-
-        console.log(condition[0], condition[1], condition[2])     // we get values of all the indexes in integer 
-
-        console.log(boxes[condition[0]], boxes[condition[1]], boxes[condition[2]])     
-        // here for example condition 0 has 1 2 3
-        // then with boxes we get - boxes ke 1st index, 2nd index and 3rd index do 
-        // we need them for comparison
-
-        console.log(
-            boxes[condition[0]].textContent, 
-            boxes[condition[1]].textContent, 
-            boxes[condition[2]].textContent
-            )     
-        // with this we can access the inner text of the box if nothing is there then no value on that index
-        */
-
-
-
-        let val1 = boxes[condition[0]].textContent;
-        let val2 = boxes[condition[1]].textContent;
-        let val3 = boxes[condition[2]].textContent;
-
-        if (val1 !== "" && val2 !== "" && val3 !== "") {
-            if (val1 === val2 && val2 === val3) {
-                console.log("winner", val1);
-
-                displayWinner(val1);
-                return;
+        for (const pattern of winPatterns) {
+            const [a, b, c] = pattern;
+            if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
+                return gameBoard[a];
             }
         }
-    }
-    if (count === 9) {
-        winner.textContent = "It's a Draw";
-        message.classList.remove("hide");
-    }
-};
 
-const resetGame = () => {
-    play_x = true;
-    count = 0;
-    startButtons();
+        return null;
+    };
 
-    // Clear text content and enable all boxes
-    boxes.forEach((box) => {
-        box.textContent = "";
-        box.disabled = false;
-        box.classList.remove('clicked');
+    // Function to change the switch 
+    function changeBg() {
+        if (currentPlayer === "X") {
+            currentPlayer = "O";
+            document.querySelector('#two').style.backgroundColor = "white";
+            document.querySelector('#one').style.backgroundColor = "#DC143C";
+        } else {
+            currentPlayer = "X";
+            document.querySelector('#one').style.backgroundColor = "white";
+            document.querySelector('#two').style.backgroundColor = "#DC143C";
+        }
+    }
+
+    // Function to check if the board is full (tie)
+    const isBoardFull = () => !gameBoard.includes('');
+
+    // Function to handle player's move
+    const handlePlayerMove = (index) => {
+        if (!gameOver && gameBoard[index] === '') {
+            gameBoard[index] = currentPlayer;
+            boxes[index].textContent = currentPlayer;
+            boxes[index].classList.add('clicked');
+
+            const winner = checkWinner();
+            if (winner) {
+                gameOver = true;
+                messageDisplay.classList.remove('hide');
+                winnerDisplay.textContent = `${winner} wins!`;
+            } else if (isBoardFull()) {
+                gameOver = true;
+                messageDisplay.classList.remove('hide');
+                winnerDisplay.textContent = 'It\'s a tie!';
+            } else {
+                changeBg();
+                // If AI is the next player, make its move
+                if (currentPlayer === playerO && playerTypeSelect.value === 'ai') {
+                    makeAIMove();
+                }
+            }
+        }
+    };
+
+    // Function to make AI move using Minimax algorithm
+    const makeAIMove = () => {
+        const bestMove = getBestMove();
+        handlePlayerMove(bestMove);
+    };
+
+    // Function to get the best move for the AI using Minimax algorithm
+    const getBestMove = () => {
+        let bestScore = -Infinity;
+        let bestMove;
+
+        for (let i = 0; i < 9; i++) {
+            if (gameBoard[i] === '') {
+                gameBoard[i] = playerO;
+                const score = minimax(gameBoard, 0, false);
+                gameBoard[i] = '';
+
+                if (score > bestScore) {
+                    bestScore = score;
+                    bestMove = i;
+                }
+            }
+        }
+
+        return bestMove;
+    };
+
+    // Minimax algorithm
+    const minimax = (board, depth, isMaximizing) => {
+        const scores = {
+            X: -1,
+            O: 1,
+            tie: 0
+        };
+
+        const winner = checkWinner();
+        if (winner) {
+            return scores[winner];
+        }
+
+        if (isBoardFull()) {
+            return scores.tie;
+        }
+
+        if (isMaximizing) {
+            let maxScore = -Infinity;
+            for (let i = 0; i < 9; i++) {
+                if (board[i] === '') {
+                    board[i] = playerO;
+                    maxScore = Math.max(maxScore, minimax(board, depth + 1, !isMaximizing));
+                    board[i] = '';
+                }
+            }
+            return maxScore;
+        } else {
+            let minScore = Infinity;
+            for (let i = 0; i < 9; i++) {
+                if (board[i] === '') {
+                    board[i] = playerX;
+                    minScore = Math.min(minScore, minimax(board, depth + 1, !isMaximizing));
+                    board[i] = '';
+                }
+            }
+            return minScore;
+        }
+    };
+
+    // Event listeners for player's moves
+    boxes.forEach((box, index) => {
+        box.addEventListener('click', () => {
+            handlePlayerMove(index);
+        });
     });
 
-    // Hide the message
-    message.classList.add('hide');
-    winner.textContent = "";
+    // Event listener for Play Again button
+    playAgainBtn.addEventListener('click', () => {
+        resetGame();
+        messageDisplay.classList.add('hide');
+    });
 
-    // Reset background colors
-    document.querySelector('#one').style.backgroundColor = "white";
-    document.querySelector('#two').style.backgroundColor = "#e32047";
-};
+    // Event listener for New Game button
+    newGameBtn.addEventListener('click', () => {
+        resetGame();
+        messageDisplay.classList.add('hide');
+    });
 
-newGame.addEventListener('click', resetGame);
-playAgain.addEventListener('click', resetGame);
+    // Function to reset the game
+    const resetGame = () => {
+        gameBoard = ['', '', '', '', '', '', '', '', ''];
+        boxes.forEach((box) => {
+            box.textContent = '';
+            box.classList.remove('clicked');
+        });
+
+        currentPlayer = playerX;
+        gameOver = false;
+
+        // Reset background colors
+        document.querySelector('#one').style.backgroundColor = "white";
+        document.querySelector('#two').style.backgroundColor = "#DC143C";
+    };
+
+    // Event listener for player type selection
+    playerTypeSelect.addEventListener('change', () => {
+        resetGame();
+        if (currentPlayer === playerO && playerTypeSelect.value === 'ai') {
+            makeAIMove();
+            changeBg(); // Ensure background color is updated after AI move
+        }
+    });
+});
+                            
